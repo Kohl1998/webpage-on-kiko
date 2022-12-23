53e02d7bd42e54783018da746eb8aa9e59ac9704
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2022-12-23 15:56:17 +0000

    fiex syntax in js file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-alert("This is my first webpage using HTML & CSS")
+alert("This is my first webpage using HTML & CSS");
 
-var validation = confirm("Do you want to view my webpage?")
+var validation = confirm("Do you want to view my webpage?");
 
-var feedback = prompt("What do you think could improve?")
+var feedback = prompt("What do you think could improve?"); 
 
 alert(validation);
 alert(feedback);
